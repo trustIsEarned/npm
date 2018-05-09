18dd1ed92a989aaa50b1756240c2f99682fd300b
Allan Barch <apbarch@gmail.com>
2018-05-08 21:34:44 -0400

    file setup

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const data = (function(){
+	if(localStorage.getItem("words") == null){
+		let arr = [{one: "milk"}, {two: "cookies"}];
+		arr = JSON.stringify(arr);
+		localStorage.setItem("words", arr);
+
+
+	}else{
+		let content = localStorage.getItem("words");
+		content = JSON.parse(content);
+		content.push({three: "peanuts"});
+		content = JSON.stringify(content)
+		localStorage.setItem("words", content);
+	}
+
+
+})();
